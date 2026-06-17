88bda7c9bccf8a73582173326717ef6c0ed87af8
Yusuf Benli <yusufbenli@gmail.com>
2026-06-17 15:57:02 +0300

    chore: recreate Flutter project platform folders (android, ios, web, etc.)

---
+++ android/app/src/main/kotlin/com/example/ekibim_nerede_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ekibim_nerede_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_secure_storage_macos
+import location
+import mobile_scanner
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+}
