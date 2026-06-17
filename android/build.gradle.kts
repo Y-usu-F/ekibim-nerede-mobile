31cc80bf5f1fbf0796ac072d65a23a293670a1a0
Yusuf Benli <yusufbenli@gmail.com>
2026-06-17 16:17:51 +0300

    last changed commits

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,35 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val configureProject = {
+        if (hasProperty("android")) {
+            val android = property("android")
+            if (android != null) {
+                val clazz = android.javaClass
+                try {
+                    // Use reflection to set compileSdkVersion to 36 for all subprojects (plugins)
+                    val method = clazz.getMethod("compileSdkVersion", Int::class.java)
+                    method.invoke(android, 36)
+                } catch (e: Exception) {
+                    // Fallback for newer AGP versions using compileSdk property
+                    try {
+                        val method = clazz.getMethod("setCompileSdk", java.lang.Integer::class.java)
+                        method.invoke(android, 36)
+                    } catch (e2: Exception) {}
+                }
+            }
+        }
+    }
+    if (state.executed) {
+        configureProject()
+    } else {
+        afterEvaluate {
+            configureProject()
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
